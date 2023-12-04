de68ec59cb92e98d186a017e2bbf6cbc170ef063
ppsasho <businessmail5545@gmail.com>
2023-12-04 20:55:58 +0100

    - JS schoolWork

---
+++ test3.js
@@ -0,0 +1,8 @@
+// let feet = prompt('Enter amount of feet: ');
+// const feetconversionRate = 0.3048;
+// let meters = feet * feetconversionRate;
+// console.log(feet, 'equals to: ', meters, 'meters.');
+let sideA = prompt('Enter side 1');
+let sideB = prompt('Enter side 2');
+let area = sideA * sideB;
+console.log('The area of the given rectangle is: ',area);
